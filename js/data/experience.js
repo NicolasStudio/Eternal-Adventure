0cedf623be783b45e678d5b8089c146bc6c52183
Nicolas de Almeida Sousa <121115391+NicolasStudio@users.noreply.github.com>
2026-08-18 10:32:32 -0300

    Update experience.js

--- js/data/experience.js
+++ js/data/experience.js
@@ -6,297 +6,297 @@ export default {
         required: 100,
     },
     3: {
-        required: 200,
+        required: 150,
     },
     4: {
-        required: 300,
+        required: 250,
     },
     5: {
-        required: 400,
-    },
-    6: {
         required: 500,
     },
+    6: {
+        required: 750,
+    },
     7: {
-        required: 600,
-    },
-    8: {
-        required: 700,
-    },
-    9: {
-        required: 800,
-    },
-    10: {
-        required: 900,
-    },
-    11: {
         required: 1000,
     },
-    12: {
-        required: 1100,
+    8: {
+        required: 1250,
     },
-    13: {
-        required: 1200,
-    },
-    14: {
-        required: 1300,
-    },
-    15: {
-        required: 1400,
-    },
-    16: {
+    9: {
         required: 1500,
     },
-    17: {
-        required: 1600,
+    10: {
+        required: 1750,
     },
-    18: {
-        required: 1700,
-    },
-    19: {
-        required: 1800,
-    },
-    20: {
-        required: 1900,
-    },
-    21: {
+    11: {
         required: 2000,
     },
-    22: {
-        required: 2100,
-    },
-    23: {
-        required: 2200,
-    },
-    24: {
-        required: 2300,
-    },
-    25: {
-        required: 2400,
-    },
-    26: {
+    12: {
         required: 2500,
     },
-    27: {
-        required: 2600,
-    },
-    28: {
-        required: 2700,
-    },
-    29: {
-        required: 2800,
-    },
-    30: {
-        required: 2900,
-    },
-    31: {
+    13: {
         required: 3000,
     },
-    32: {
-        required: 3100,
-    },
-    33: {
-        required: 3200,
-    },
-    34: {
-        required: 3300,
-    },
-    35: {
-        required: 3400,
-    },
-    36: {
+    14: {
         required: 3500,
     },
-    37: {
-        required: 3600,
-    },
-    38: {
-        required: 3700,
-    },
-    39: {
-        required: 3800,
-    },
-    40: {
-        required: 3900,
-    },
-    41: {
+    15: {
         required: 4000,
     },
-    42: {
-        required: 4100,
-    },
-    43: {
-        required: 4200,
-    },
-    44: {
-        required: 4300,
-    },
-    45: {
-        required: 4400,
-    },
-    46: {
+    16: {
         required: 4500,
     },
-    47: {
-        required: 4600,
-    },
-    48: {
-        required: 4700,
-    },
-    49: {
-        required: 4800,
-    },
-    50: {
-        required: 4900,
-    },
-    51: {
+    17: {
         required: 5000,
     },
-    52: {
-        required: 5100,
-    },
-    53: {
-        required: 5200,
-    },
-    54: {
-        required: 5300,
-    },
-    55: {
-        required: 5400,
-    },
-    56: {
+    18: {
         required: 5500,
     },
-    57: {
-        required: 5600,
-    },
-    58: {
-        required: 5700,
-    },
-    59: {
-        required: 5800,
-    },
-    60: {
-        required: 5900,
-    },
-    61: {
+    19: {
         required: 6000,
     },
-    62: {
-        required: 6100,
-    },
-    63: {
-        required: 6200,
-    },
-    64: {
-        required: 6300,
-    },
-    65: {
-        required: 6400,
-    },
-    66: {
+    20: {
         required: 6500,
     },
-    67: {
-        required: 6600,
-    },
-    68: {
-        required: 6700,
-    },
-    69: {
-        required: 6800,
-    },
-    70: {
-        required: 6900,
-    },
-    71: {
+    21: {
         required: 7000,
     },
+    22: {
+        required: 7500,
+    },
+    23: {
+        required: 8000,
+    },
+    24: {
+        required: 8500,
+    },
+    25: {
+        required: 9000,
+    },
+    26: {
+        required: 9500,
+    },
+    27: {
+        required: 10000,
+    },
+    28: {
+        required: 11000,
+    },
+    29: {
+        required: 12000,
+    },
+    30: {
+        required: 13000,
+    },
+    31: {
+        required: 14000,
+    },
+    32: {
+        required: 15000,
+    },
+    33: {
+        required: 16000,
+    },
+    34: {
+        required: 17000,
+    },
+    35: {
+        required: 18000,
+    },
+    36: {
+        required: 19000,
+    },
+    37: {
+        required: 20000,
+    },
+    38: {
+        required: 21500,
+    },
+    39: {
+        required: 22000,
+    },
+    40: {
+        required: 22500,
+    },
+    41: {
+        required: 23000,
+    },
+    42: {
+        required: 23500,
+    },
+    43: {
+        required: 24000,
+    },
+    44: {
+        required: 24500,
+    },
+    45: {
+        required: 25000,
+    },
+    46: {
+        required: 25500,
+    },
+    47: {
+        required: 26000,
+    },
+    48: {
+        required: 26500,
+    },
+    49: {
+        required: 27000,
+    },
+    50: {
+        required: 27500,
+    },
+    51: {
+        required: 28000,
+    },
+    52: {
+        required: 28500,
+    },
+    53: {
+        required: 29000,
+    },
+    54: {
+        required: 29500,
+    },
+    55: {
+        required: 30000,
+    },
+    56: {
+        required: 30500,
+    },
+    57: {
+        required: 31000,
+    },
+    58: {
+        required: 31500,
+    },
+    59: {
+        required: 32000,
+    },
+    60: {
+        required: 32500,
+    },
+    61: {
+        required: 33000,
+    },
+    62: {
+        required: 33500,
+    },
+    63: {
+        required: 34000,
+    },
+    64: {
+        required: 34500,
+    },
+    65: {
+        required: 35000,
+    },
+    66: {
+        required: 35500,
+    },
+    67: {
+        required: 36000,
+    },
+    68: {
+        required: 36500,
+    },
+    69: {
+        required: 37000,
+    },
+    70: {
+        required: 37500,
+    },
+    71: {
+        required: 38000,
+    },
     72: {
-        required: 7240,
+        required: 38500,
     },
     73: {
-        required: 7490,
+        required: 39000,
     },
     74: {
-        required: 7740,
+        required: 39500,
     },
     75: {
-        required: 7990,
+        required: 40000,
     },
     76: {
-        required: 8250,
+        required: 40500,
     },
     77: {
-        required: 8510,
+        required: 41000,
     },
     78: {
-        required: 8780,
+        required: 41500,
     },
     79: {
-        required: 9050,
+        required: 42000,
     },
     80: {
-        required: 9320,
+        required: 42500,
     },
     81: {
-        required: 9600,
+        required: 43000,
     },
     82: {
-        required: 9880,
+        required: 43500,
     },
     83: {
-        required: 10170,
+        required: 44000,
     },
     84: {
-        required: 10460,
+        required: 44500,
     },
     85: {
-        required: 10750,
+        required: 45000,
     },
     86: {
-        required: 11050,
+        required: 45500,
     },
     87: {
-        required: 11350,
+        required: 46000,
     },
     88: {
-        required: 11660,
+        required: 46500,
     },
     89: {
-        required: 11970,
+        required: 47000,
     },
     90: {
-        required: 12280,
+        required: 47500,
     },
     91: {
-        required: 12600,
+        required: 48000,
     },
     92: {
-        required: 12920,
+        required: 48500,
     },
     93: {
-        required: 13250,
+        required: 49000,
     },
     94: {
-        required: 13580,
+        required: 49500,
     },
     95: {
-        required: 13910,
+        required: 50000,
     },
     96: {
-        required: 14250,
+        required: 50500,
     },
     97: {
-        required: 14590,
+        required: 51000,
     },
     98: {
-        required: 14940,
+        required: 51500,
     },
     99: {
-        required: 15290,
+        required: 52000,
     },
     100: {
-        required: 15640,
+        required: 52500,
     },
 };
